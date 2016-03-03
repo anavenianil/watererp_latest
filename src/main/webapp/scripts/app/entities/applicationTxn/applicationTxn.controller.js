e67638490478efb697327a7ff412520c7b5b904d
mohib <mohibullahkhan@callippus.co.uk>
2016-03-03 14:27:53 +0530

    customer form combined in applicationTxn form

--- src/main/webapp/scripts/app/entities/applicationTxn/applicationTxn.controller.js
+++ src/main/webapp/scripts/app/entities/applicationTxn/applicationTxn.controller.js
@@ -1,13 +1,28 @@
 'use strict';
 
 angular.module('watererpApp')
-    .controller('ApplicationTxnController', function ($scope, $state, ApplicationTxn, ParseLinks) {
+    .controller('ApplicationTxnController', function ($scope, $state, ApplicationTxn, ParseLinks, Customer, ApplicationTypeMaster, ConnectionTypeMaster, 
+    		CtegoryMaster, SewerSize,FileNumber, PipeSizeMaster) {
 
-        $scope.applicationTxns = [];
+        //$scope.applicationTxns = [];
         $scope.predicate = 'id';
         $scope.reverse = true;
         $scope.page = 0;
+        
+        $scope.applicationtypemasters = ApplicationTypeMaster.query();
+        $scope.connectiontypemasters = ConnectionTypeMaster.query();
+        $scope.ctegorymasters = CtegoryMaster.query();
+        $scope.pipesizemasters = PipeSizeMaster.query();
+        $scope.sewersizes = SewerSize.query();
+        $scope.filenumbers = FileNumber.query();
+        $scope.applicationTxn = {};
+        $scope.customer = {};
+        
+        
         $scope.loadAll = function() {
+        	alert("hello");
+        	$scope.applicationTxns = [];
+        	$('#viewApplicationTxnModal').modal('show');
             ApplicationTxn.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
                 $scope.links = ParseLinks.parse(headers('link'));
                 for (var i = 0; i < result.length; i++) {
@@ -18,13 +33,13 @@ angular.module('watererpApp')
         $scope.reset = function() {
             $scope.page = 0;
             $scope.applicationTxns = [];
-            $scope.loadAll();
+            //$scope.loadAll();
         };
         $scope.loadPage = function(page) {
             $scope.page = page;
-            $scope.loadAll();
+            //$scope.loadAll();
         };
-        $scope.loadAll();
+        //$scope.loadAll();
 
 
         $scope.refresh = function () {
@@ -56,5 +71,83 @@ angular.module('watererpApp')
                 status: null,
                 id: null
             };
+        };
+        
+        //save method
+        var onSaveSuccess = function (result) {
+            $scope.$emit('watererpApp:applicationTxnUpdate', result);
+            $scope.$emit('watererpApp:customerUpdate', result);
+            //$uibModalInstance.close(result);
+            $scope.isSaving = false;
+        };
+
+        var onSaveError = function (result) {
+            $scope.isSaving = false;
+        };
+
+        $scope.save = function () {
+            $scope.isSaving = true;
+            if ($scope.applicationTxn.id != null) {
+                ApplicationTxn.update($scope.applicationTxn, onSaveSuccess, onSaveError);
+            } else {
+                ApplicationTxn.save($scope.applicationTxn, onSaveSuccess, onSaveError);
+            }
+            if ($scope.customer.id != null) {
+                Customer.update($scope.customer, onSaveSuccess, onSaveError);
+            } else {
+                Customer.save($scope.customer, onSaveSuccess, onSaveError);
+            }
+        };
+        
+        //
+        /*var onSaveSuccess = function (result) {
+            $scope.$emit('watererpApp:customerUpdate', result);
+            $uibModalInstance.close(result);
+            $scope.isSaving = false;
+        };
+
+        var onSaveError = function (result) {
+            $scope.isSaving = false;
+        };
+
+        $scope.save = function () {
+            $scope.isSaving = true;
+            if ($scope.customer.id != null) {
+                Customer.update($scope.customer, onSaveSuccess, onSaveError);
+            } else {
+                Customer.save($scope.customer, onSaveSuccess, onSaveError);
+            }
+        };*/
+        
+        
+        //date picker for customer Entity
+        $scope.datePickerForRequestDate = {};
+
+        $scope.datePickerForRequestDate.status = {
+            opened: false
+        };
+
+        $scope.datePickerForRequestDateOpen = function($event) {
+            $scope.datePickerForRequestDate.status.opened = true;
+        };
+        
+        //date picker for ApplicationTxn Entity
+        $scope.datePickerForCreatedDate = {};
+
+        $scope.datePickerForCreatedDate.status = {
+            opened: false
+        };
+
+        $scope.datePickerForCreatedDateOpen = function($event) {
+            $scope.datePickerForCreatedDate.status.opened = true;
+        };
+        $scope.datePickerForUpdatedDate = {};
+
+        $scope.datePickerForUpdatedDate.status = {
+            opened: false
+        };
+
+        $scope.datePickerForUpdatedDateOpen = function($event) {
+            $scope.datePickerForUpdatedDate.status.opened = true;
         };
     });
--- src/main/webapp/scripts/app/entities/sewerSize/sewerSize-delete-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/sewerSize/sewerSize-delete-dialog.controller.js
@@ -13,5 +13,4 @@ angular.module('watererpApp')
                     $uibModalInstance.close(true);
                 });
         };
-
     });
