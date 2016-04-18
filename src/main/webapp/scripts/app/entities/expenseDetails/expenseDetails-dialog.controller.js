8684fa26c0162364d5db557d84583d1f3043df7f
Bhaskar Reddy <bhaskarreddy99999@gmail.com>
2016-04-18 13:36:05 +0530

    Doing changes in Expense Details

--- src/main/webapp/scripts/app/entities/expenseDetails/expenseDetails-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/expenseDetails/expenseDetails-dialog.controller.js
@@ -54,103 +54,14 @@ angular.module('watererpApp')
         $scope.datePickerForInstrDtOpen = function($event) {
             $scope.datePickerForInstrDt.status.opened = true;
         };
+        
+        $scope.resetInstr = function(paymentMode) {
+			if (paymentMode === 'CASH') {
+				$scope.instrEnabled = false;
+				$scope.collDetails.instrNo = null;
+				$scope.collDetails.instrDt = null;
+				$scope.collDetails.instrIssuer = null;
+			} else
+				$scope.instrEnabled = true;
+		}
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*'use strict';
-
-angular.module('watererpApp').controller('ExpenseDetailsDialogController',
-    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'ExpenseDetails', 'PaymentTypes', 'InstrumentIssuerMaster', 'CollectionTypeMaster',
-        function($scope, $stateParams, $uibModalInstance, entity, ExpenseDetails, PaymentTypes, InstrumentIssuerMaster, CollectionTypeMaster) {
-
-        $scope.expenseDetails = entity;
-        $scope.paymenttypess = PaymentTypes.query();
-        $scope.instrumentissuermasters = InstrumentIssuerMaster.query();
-        $scope.collectiontypemasters = CollectionTypeMaster.query();
-        $scope.load = function(id) {
-            ExpenseDetails.get({id : id}, function(result) {
-                $scope.expenseDetails = result;
-            });
-        };
-
-        var onSaveSuccess = function (result) {
-            $scope.$emit('watererpApp:expenseDetailsUpdate', result);
-            $uibModalInstance.close(result);
-            $scope.isSaving = false;
-        };
-
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
-
-        $scope.save = function () {
-            $scope.isSaving = true;
-            if ($scope.expenseDetails.id != null) {
-                ExpenseDetails.update($scope.expenseDetails, onSaveSuccess, onSaveError);
-            } else {
-                ExpenseDetails.save($scope.expenseDetails, onSaveSuccess, onSaveError);
-            }
-        };
-
-        $scope.clear = function() {
-            $uibModalInstance.dismiss('cancel');
-        };
-        $scope.datePickerForExpenseDt = {};
-
-        $scope.datePickerForExpenseDt.status = {
-            opened: false
-        };
-
-        $scope.datePickerForExpenseDtOpen = function($event) {
-            $scope.datePickerForExpenseDt.status.opened = true;
-        };
-        $scope.datePickerForInstrDt = {};
-
-        $scope.datePickerForInstrDt.status = {
-            opened: false
-        };
-
-        $scope.datePickerForInstrDtOpen = function($event) {
-            $scope.datePickerForInstrDt.status.opened = true;
-        };
-}]);
-*/
