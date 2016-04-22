695e299b78642a39a08ee3241f43757cc4cbfd8f
Srinivas Gattu <srinivas@callippus.co.uk>
2016-04-22 15:43:48 +0530

    Modularisation mostly working.

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -5,44 +5,13 @@ angular
 		.controller(
 				'NavbarController',
 				function($rootScope, $scope, $http, $location, $state, Auth,
-						Principal) {
+						Principal, Navbar) {
 					$scope.isAuthenticated = Principal.isAuthenticated;
 					$scope.$state = $state;
 					$scope.module2MenuItems = {};
 					$scope.module = {};
 					$scope.orgRole = {};
-					//$scope.inProduction = ENV === 'prod';
-
-					$scope.getCurrentModule = function() {
-						var path = $location.absUrl()
-						var moduleName = "";
-						console.log("This is the path:" + path)
-
-						//if path="http://localhost:8080/billingAndCollectionMain#/"
-						//below will give "billingAndCollectionMain#"
-						var parts = path.split("/");
-						if (parts[parts.length - 1].length == 0) {
-							moduleName = parts[parts.length - 2];
-						} else {
-							moduleName = parts[parts.length - 1];
-						}
-						//console.log(moduleName); //"billingAndCollectionMain#"
-
-						if (moduleName.charAt(moduleName.length - 1) == '#') {
-							moduleName = moduleName.slice(0, -1);
-							var matches = Principal.geModuleMenus().filter(function(val, index, array) {
-							    return val.server_url === moduleName;
-							});
-
-							console.log(matches);
-							
-							$scope.module = matches[0];
-						} else {
-							moduleName = "";
-							$scope.module = {};
-						}
-
-					}
+					// $scope.inProduction = ENV === 'prod';
 
 					$scope.getByKey = function(key) {
 						var found = null;
@@ -68,7 +37,7 @@ angular
 					$scope.getLogin = function() {
 						$scope.user = Principal.getLogonUser();
 						$scope.orgRole = Principal.getOrgRole();
-						//console.log("User is: "+JSON.stringify($scope.user));
+						// console.log("User is: "+JSON.stringify($scope.user));
 						if ($scope.user == null) {
 							$scope.navbarUserId = "";
 							$scope.userRole = "";
@@ -78,7 +47,7 @@ angular
 									+ $scope.user.login + ")";
 							$scope.userRole = $scope.orgRole.orgRoleName;
 						}
-						//for navbar module and menu_items
+						// for navbar module and menu_items
 						if ($scope.isAuthenticated()) {
 							$scope.module2MenuItems = Principal.geModuleMenus();
 						} else
@@ -87,6 +56,6 @@ angular
 						return $scope.isAuthenticated();
 					}
 
-					$scope.getCurrentModule();
+					$scope.module = Navbar.getCurrentModule();
 
 				});
---
+++ src/main/webapp/scripts/components/navbar/navbar.service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('watererpApp')
+    .factory('Navbar', function Auth($rootScope, $state, $q, Principal, $location) {
+        return {
+
+			getCurrentModule: function() {
+				var path = $location.absUrl()
+				var moduleName = "";
+				//console.log("This is the path:" + path)
+
+				// if path="http://localhost:8080/billingAndCollectionMain#/"
+				// below will give "billingAndCollectionMain"
+				var main = path.split("#")[0];
+				//console.log("This is the main url:" + main)
+				var parts = main.split("/");
+				if (parts[parts.length - 1].length == 0) {
+					moduleName = parts[parts.length - 2];
+				} else {
+					moduleName = parts[parts.length - 1];
+				}
+				//console.log(moduleName); // "billingAndCollectionMain"
+
+				var matches = Principal.geModuleMenus().filter(
+						function(val, index, array) {
+							return val.server_url === moduleName;
+						});
+
+				//console.log(matches);
+
+				return matches[0];
+			}
+
+        };
+    });
