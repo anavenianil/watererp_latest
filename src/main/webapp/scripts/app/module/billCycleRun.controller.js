525ae0b5df6e030717122844efba16bb8ae1c2fd
Srinivas Gattu <srinivas@callippus.co.uk>
2016-04-23 14:27:43 +0530

    Mostly integrated. Connection dashboard in progress.

--- src/main/webapp/scripts/app/module/billCycleRun.controller.js
+++ src/main/webapp/scripts/app/module/billCycleRun.controller.js
@@ -2,29 +2,5 @@
 
 angular.module('watererpApp').controller('BillCycleRunController',
 		function($scope, $state, $rootScope, Account, User, Principal) {
-			$scope.pendingRequests = [];
-			$scope.approvedRequests = [];
-			$scope.myRequests = [];
-
-			$scope.loadAll = function() {
-
-			}
-
-			Principal.identity().then(function(account) {
-				$scope.account = account;
-				$scope.isAuthenticated = Principal.isAuthenticated;
-
-				if (!$scope.isAuthenticated())
-					$state.go('login');
-
-				else {
-					User.get({
-						login : $scope.account.login
-					}, function(result) {
-						$scope.user = result;
-
-					});
-					$scope.loadAll();
-				}
-			});
+			$state.go('billRunMaster');
 		});
