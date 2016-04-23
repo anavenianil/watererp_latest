9596746900adc1ff25c7b85e2b22d2279fc8081d
Srinivas Gattu <srinivas@callippus.co.uk>
2016-04-23 15:22:53 +0530

    Minor changes to connection module

--- src/main/webapp/scripts/app/module/connection.controller.js
+++ src/main/webapp/scripts/app/module/connection.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('watererpApp').controller(
-		'MainController',
+		'ConnectionController',
 		function($scope, $state, $rootScope, Account, User,
 				ApplicationTxnService, Principal) {
 			$scope.pendingRequests = [];
