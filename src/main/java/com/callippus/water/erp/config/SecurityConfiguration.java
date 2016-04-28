dcdad28fd86e5cb50775f7dc727655314dd77d56
Srinivas Gattu <srinivas@callippus.co.uk>
2016-04-28 11:57:20 +0530

    Changed CSRF settings for enabling Unified Payment API call without CSRF

--- src/main/java/com/callippus/water/erp/config/SecurityConfiguration.java
+++ src/main/java/com/callippus/water/erp/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.callippus.water.erp.config;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +25,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.callippus.water.erp.security.AjaxAuthenticationFailureHandler;
 import com.callippus.water.erp.security.AjaxAuthenticationSuccessHandler;
@@ -88,6 +92,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
+				.requireCsrfProtectionMatcher(new RequestMatcher() {
+					// private Pattern allowedMethods =
+					// Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+					private RegexRequestMatcher apiMatcher = new RegexRequestMatcher(
+							"/api/unifiedPGResponse", null);
+
+					@Override
+					public boolean matches(HttpServletRequest request) {
+						// No CSRF due to allowedMethod
+						// if(allowedMethods.matcher(request.getMethod()).matches())
+						// return false;
+
+						// No CSRF due to api call
+						if (apiMatcher.matches(request))
+							return false;
+
+						// CSRF for everything else that is not an API call or
+						// an allowedMethod
+						return true;
+					}
+				})
 				.and()
 				.addFilterAfter(new CsrfCookieGeneratorFilter(),
 						CsrfFilter.class).exceptionHandling()
