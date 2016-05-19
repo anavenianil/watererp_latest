6c504759215cdd75946b4c80d2d2d44bd62053a5
Srinivas Gattu <srinivas@callippus.co.uk>
2016-05-19 17:43:42 +0530

    Ready for real test with Jenkins.

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@
 #grunt test
 
 ##### Integration Testing #######
-sudo SPRING_PROFILES_ACTIVE=fast /var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true test
-
+#sudo SPRING_PROFILES_ACTIVE=fast /var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true test
 ##### E2E Testing ###############
 a=`ps -ef|grep java|grep spring-boot|awk '{print $2}'`
 if [ -n "$a" ]; then
