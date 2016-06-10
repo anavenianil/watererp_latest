14d9ad04696069a532313eef09037c2812fe1e36
Srinivas Gattu <srinivas@callippus.co.uk>
2016-06-10 12:38:08 +0530

    Changed script slightly.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #This facilitates run in Jenkins
-
+echo "Running script from PWD:" `pwd`
 ##### Unit Testing ##############
 #grunt test
 
 ##### Integration Testing #######
-#sudo SPRING_PROFILES_ACTIVE=fast /var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true test
-sudo SPRING_PROFILES_ACTIVE=fast /var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true  -Dtest=BillRunMasterResourceIntTest test
+#sudo SPRING_PROFILES_ACTIVE=fast ../../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true test
+sudo SPRING_PROFILES_ACTIVE=fast ../../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dmaven.test.failure.ignore=true  -Dtest=BillRunMasterResourceIntTest test
 ##### E2E Testing ###############
 a=`ps -ef|grep java|grep spring-boot|awk '{print $2}'`
 if [ -n "$a" ]; then
@@ -13,7 +13,7 @@ if [ -n "$a" ]; then
 	sudo kill -9 $a
 fi
 pwd
-sudo BUILD_ID=dontKillMe nohup /var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Pfast spring-boot:run  > /var/log/jenkins/app.log&
+sudo BUILD_ID=dontKillMe nohup ../../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Pfast spring-boot:run  > /var/log/jenkins/app.log&
 #sleep 60
 #/var/lib/jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/Node/bin/npm install
 #sudo grunt protractor-xvfb
