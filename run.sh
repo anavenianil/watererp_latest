e9b3329ce8e745e76a4085fda22ccd4c89872cd3
Srinivas Gattu <srinivas@callippus.co.uk>
2016-06-13 08:19:39 +0530

    Removed path in run script. Configured in Jenkins>System>Tools Location

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Running script from PWD:" `pwd`
 ##### Unit Testing ##############
 #grunt test
 
-sudo ../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn initialize
+sudo mvn clean initialize
 
 message=$(grep git.commit.message.full target/classes/config/git.properties | sed -n -e "s/.*=//p")
 
@@ -26,8 +26,8 @@ fi
 set -e
 
 ##### Integration Testing #######
-#sudo SPRING_PROFILES_ACTIVE=fast ../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn test
-sudo SPRING_PROFILES_ACTIVE=fast ../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Dtest=BillRunMasterResourceIntTest test
+#sudo SPRING_PROFILES_ACTIVE=fast mvn test
+sudo SPRING_PROFILES_ACTIVE=fast mvn -Dtest=BillRunMasterResourceIntTest test
 ##### E2E Testing ###############
 a=`ps -ef|grep java|grep spring-boot|awk '{print $2}'`
 if [ -n "$a" ]; then
@@ -35,7 +35,7 @@ if [ -n "$a" ]; then
 	sudo kill -9 $a
 fi
 pwd
-sudo BUILD_ID=dontKillMe nohup ../../tools/hudson.tasks.Maven_MavenInstallation/Maven/bin/mvn -Pfast spring-boot:run  > /var/log/jenkins/app.log&
+sudo BUILD_ID=dontKillMe nohup mvn -Pfast spring-boot:run  > /var/log/jenkins/app.log&
 #sleep 60
-#/var/lib/jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/Node/bin/npm install
+#npm install
 #sudo grunt protractor-xvfb
